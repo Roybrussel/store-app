86aa2e2df65752b8bb0853d30551a7ce3e0f3a0d
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-04-01 15:26:01 +0200

    Created a detail class component for practice as well.

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,8 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export default function useFetch(url) {
+	const isMounted = useRef(false);
 	const [
 		data,
 		setData
@@ -18,22 +19,27 @@ export default function useFetch(url) {
 
 	useEffect(
 		() => {
+			isMounted.current = true;
 			async function init() {
 				try {
 					const response = await fetch(baseUrl + url);
 					if (response.ok) {
 						const json = await response.json();
-						setData(json);
+						if (isMounted.current) setData(json);
 					} else {
 						throw response;
 					}
 				} catch (e) {
-					setError(e);
+					if (isMounted.current) setError(e);
 				} finally {
-					setLoading(false);
+					if (isMounted.current) setLoading(false);
 				}
 			}
 			init();
+
+			return () => {
+				isMounted.current = false;
+			};
 		},
 		[
 			url
@@ -41,4 +47,9 @@ export default function useFetch(url) {
 	);
 
 	return { data, error, loading };
+}
+
+export function Fetch({ url, children }) {
+	const { data, loading, error } = useFetch(url);
+	return children(data, loading, error);
 }
