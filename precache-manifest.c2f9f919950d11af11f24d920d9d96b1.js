c472868b04e4cfd0c3f36efc54514f8131dd4a38
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-04-02 12:43:53 +0200

    Updates

---
+++ precache-manifest.c2f9f919950d11af11f24d920d9d96b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfb62cd078dc80baeec4e4ccff3bf19",
+    "url": "/store-app/index.html"
+  },
+  {
+    "revision": "72a1ddd906b25aebe404",
+    "url": "/store-app/static/css/main.4ecbd5e7.chunk.css"
+  },
+  {
+    "revision": "393faab25e91b802a0d3",
+    "url": "/store-app/static/js/2.736ddffb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/store-app/static/js/2.736ddffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a1ddd906b25aebe404",
+    "url": "/store-app/static/js/main.18422fbf.chunk.js"
+  },
+  {
+    "revision": "46b906ca3f392e9d5ce3",
+    "url": "/store-app/static/js/runtime-main.d712f5d8.js"
+  }
+]);
